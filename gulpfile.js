7b4273d899d365be104d61a7e37b6364ca77bb58
Jakariya06 <muhammadjakariya177@gmail.com>
2021-10-15 15:41:18 +0700

    purging css sisakan yg berguna

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const {src, dest, watch, series} = require('gulp')
 const sass = require('gulp-sass') (require('sass'))
+const purgecss = require('gulp-purgecss')
 
 function createStyles() {
     return src('itachi/**/*.scss')
      .pipe(sass())
+     .pipe(purgecss({ content : ['*.html']}))
      .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['itachi/**/*.scss'], createStyles)
+    watch(['itachi/**/*.scss', '*.html'], createStyles)
 }
 
 exports.default = series(createStyles, watchTask)
